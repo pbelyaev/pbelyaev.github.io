1e69a5697a90f036e5222c57d86d6aa3073616fe
Pavel Beliaev <ksyddd@gmail.com>
2021-03-05 23:17:19 +0100

    Embarrassing grammar mistakes removed

--- src/components/BoringMessage.tsx
+++ src/components/BoringMessage.tsx
@@ -5,11 +5,11 @@ export const BoringMessage: FunctionComponent = () => {
     <section className="container mx-auto flex flex-col px-5 py-24 justify-center items-center text-gray-700">
       <div className="w-full md:w-2/3 text-center">
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-          Interesting things are comming
+          Interesting things are coming
         </h1>
         <p className="mb-8 leading-relaxed">
           Something interesting should appear on this page but until it does
-          this message and a few social links are here to intertain you.
+          this message and a few social links are here to entertain you.
         </p>
       </div>
     </section>
