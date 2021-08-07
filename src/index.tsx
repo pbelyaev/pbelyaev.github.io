f18f857f07e459fa1107956171060d88babe6312
Pavel Beliaev <pbelyaev@users.noreply.github.com>
2021-08-07 22:29:27 +0000

    Renamed components

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { render } from 'react-dom';
 
 import './tailwind.css';
-import { App } from './App';
+import { App } from './app';
 
 render(
   <StrictMode>
