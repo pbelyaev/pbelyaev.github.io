2790c9e2cdb322f9211f943fa883e1a013713c4c
Pavel Beliaev <pbelyaev@users.noreply.github.com>
2021-08-08 00:29:57 +0200

    Renamed components (#18)

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import { FunctionComponent } from 'react';
 
-import { Header } from './components/Header';
-import { BoringMessage } from './components/BoringMessage';
-import { Footer } from './components/Footer';
+import { Header } from './components/header';
+import { BoringMessage } from './components/boring-message';
+import { Footer } from './components/footer';
 
 export const App: FunctionComponent = () => {
   return (
