20e2d98aa6bba5a6534af2e9e4e775a6cc1d2db2
pbelyaev <pbelyaev@users.noreply.github.com>
2021-08-07 22:31:04 +0000

    Deploying to github-page from  @ 2790c9e2cdb322f9211f943fa883e1a013713c4c 🚀

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,6 @@
-import { Header } from './components/Header.js';
-import { BoringMessage } from './components/BoringMessage.js';
-import { Footer } from './components/Footer.js';
+import { Header } from './components/header.js';
+import { BoringMessage } from './components/boring-message.js';
+import { Footer } from './components/footer.js';
 import { jsx as _jsx } from "../_snowpack/pkg/react/jsx-runtime.js";
 import { Fragment as _Fragment } from "../_snowpack/pkg/react/jsx-runtime.js";
 import { jsxs as _jsxs } from "../_snowpack/pkg/react/jsx-runtime.js";
--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ import.meta.env = __SNOWPACK_ENV__;
 import { StrictMode } from '../_snowpack/pkg/react.js';
 import { render } from '../_snowpack/pkg/react-dom.js';
 import './tailwind.css.proxy.js';
-import { App } from './App.js';
+import { App } from './app.js';
 import { jsx as _jsx } from "../_snowpack/pkg/react/jsx-runtime.js";
 render( /*#__PURE__*/_jsx(StrictMode, {
   children: /*#__PURE__*/_jsx(App, {})
